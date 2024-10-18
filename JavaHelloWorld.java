45043113055c76ef0067d6ebeacf804a853aa645
Alejandro Ortiz <117293834+AlejandroAlbertoGuardadoOrtiz@users.noreply.github.com>
2024-10-18 17:53:46 +0200

    Add files via upload
    Código en "Java" para mostrar un HELLO WORLD

---
+++ JavaHelloWorld.java
@@ -0,0 +1,9 @@
+package cat.alejandro_guardado.uf1.pt1.agenda.modelo;
+
+public class JavaHelloWorld {
+	
+	public static void main(String[] argumentos){
+		System.out.println("Hola mundo");
+		}
+
+}
